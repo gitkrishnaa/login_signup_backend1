660e0bf420a4bc62c55ea7dd1d0ee3da27dcd561
krishna kumar <kri821084@gmail.com>
2024-01-04 11:40:02 +0530

    working ion payment using razorpay

--- src/model/payments/transactions.js
+++ src/model/payments/transactions.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const { model_names_obj } = require("../Model_obj");
 
 const schema=mongoose.Schema({
-payments_id:{type:mongoose.Types.ObjectId,ref:"user",},
+payments_id:{type:mongoose.Types.ObjectId,ref:"user"},
 
 })
 const model = mongoose.model(model_names_obj.transactions,schema );
